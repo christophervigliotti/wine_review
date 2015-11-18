abf2ec165c5852fb15277705d6e9928bce9b260d
Christopher Vigliotti <christopher@christophervigliotti.com>
2015-11-18 17:32:42 -0500

    homework 03 complete

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,13 +1,50 @@
-=begin
-WinesController
-must have index, show, new, create, edit, and update methods
-the actions must work with Wine objects from the database
-=end
 class WinesController < ApplicationController
+
+  before_action :set_wine, only: [ :show, :edit, :update, :destroy ]
+
   def index
+    @available_at = Time.now
     @wines = Wine.all
   end
+
   def show
+    # replaced by the before_action @wine = Wine.find(params[:id])
+  end
+
+  def new
+    @wine = Wine.new
+  end
+
+  def create
+    @wine = Wine.new(wine_params)
+    @wine.save
+    redirect_to @wine
+  end
+
+  def edit
+    # replaced by the before_action @wine = Wine.find(params[:id])
+  end
+
+  def update
+    # replaced by the before_action @wine = Wine.find(params[:id])
+    @wine.update(wine_params)
+    redirect_to @wine
+  end
+
+  def destroy
+    # replaced by the before_action @wine = Wine.find(params[:id])
+    @wine.destroy
+    redirect_to wines_url
+  end
+
+  private
+
+  def set_wine
     @wine = Wine.find(params[:id])
   end
+
+  def wine_params
+    params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
+  end  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
-=begin
-routes.rb
-change to use resource-based routing (RESTful routes)git st
-=end
 Rails.application.routes.draw do
   get '/' => 'wines#index'
-  get '/wines' => 'wines#index'
-  get '/wines/:id' => 'wines#show', as: 'wine'
+  resources :wines
 end
